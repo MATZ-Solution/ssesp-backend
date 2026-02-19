611d7ab3cfc867b42e37098d42840d1c7c634546
sohaibnayyar721 <sohaibnayyar.721@gmail.com>
2026-02-19 14:20:14 +0500

    changes in app.js

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use((req, res, next) => {
 // app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile)); // This comes after app is initialized
 
 app.get("/", (req, res) => {
-  res.send("Welcome SSESP! 17-02-2025 again 7");
+  res.send("Welcome SSESP! 19-02-2025 again 8");
 });
 
 getConnectionFromPool();
